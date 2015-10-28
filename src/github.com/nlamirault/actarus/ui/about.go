77b8a7e76bc72049f2e9bd2c9189725fd32b4dda
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2015-10-28 16:39:34 +0100

    Add dialogs UI
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

---
+++ src/github.com/nlamirault/actarus/ui/about.go
@@ -0,0 +1,73 @@
+// Copyright (C) 2015 Nicolas Lamirault <nicolas.lamirault@gmail.com>
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ui
+
+import (
+	"fmt"
+
+	"github.com/mattn/go-gtk/gtk"
+
+	"github.com/nlamirault/actarus/version"
+)
+
+var (
+	authors []string
+)
+
+func init() {
+	authors = []string{
+		"Nicolas Lamirault <nicolas.lamirault@gmail.com>",
+	}
+}
+
+// AboutDialog defines a dialog window
+type AboutDialog struct {
+	Dialog *gtk.AboutDialog
+}
+
+func NewAboutDialog(parent *gtk.Window) {
+	dialog := gtk.NewAboutDialog()
+	dialog.SetName("Actarus")
+	dialog.SetProgramName(fmt.Sprintf("Actarus - v%s", version.Version))
+	dialog.SetAuthors(authors)
+	dialog.SetLicense(`
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+	  http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+	`)
+	dialog.SetWrapLicense(true)
+	dialog.Run()
+	dialog.Destroy()
+
+	// 	dialog := gtk.NewMessageDialog(
+	// 		parent,
+	// 		gtk.DIALOG_MODAL,
+	// 		gtk.MESSAGE_INFO,
+	// 		gtk.BUTTONS_OK,
+	// 		`Actarus
+	// Copyright (c) 2015 Nicolas Lamirault <nicolas.lamirault@gmail.com>`)
+	// 	dialog.Response(func() {
+	// 		dialog.Destroy()
+	// 	})
+	dialog.Run()
+}
---
+++ src/github.com/nlamirault/actarus/ui/dialog.go
@@ -0,0 +1,44 @@
+// Copyright (C) 2015 Nicolas Lamirault <nicolas.lamirault@gmail.com>
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ui
+
+import (
+	"github.com/mattn/go-gtk/gtk"
+)
+
+func dialog(parent *gtk.Window, msg string, t gtk.MessageType) *gtk.MessageDialog {
+	messagedialog := gtk.NewMessageDialog(
+		parent,
+		gtk.DIALOG_MODAL,
+		t,
+		gtk.BUTTONS_OK,
+		msg)
+	messagedialog.Response(func() {
+		messagedialog.Destroy()
+	})
+	return messagedialog
+}
+
+// ErrorDialog display a window for error messages
+func ErrorDialog(parent *gtk.Window, msg string) {
+	dialog := dialog(parent, msg, gtk.MESSAGE_ERROR)
+	dialog.Run()
+}
+
+// InfoDialog display a window for error messages
+func InfoDialog(parent *gtk.Window, msg string) {
+	dialog := dialog(parent, msg, gtk.MESSAGE_INFO)
+	dialog.Run()
+}
